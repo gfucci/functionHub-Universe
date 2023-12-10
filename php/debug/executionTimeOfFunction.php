8c95c1802a4fed3af5fbdb8f3d77e64091a5d474
gfucci <gfucci_2005@hotmail.com>
2023-12-10 17:57:31 -0300

    Modify return message of executionTimeOfFunction

--- php/debug/executionTimeOfFunction.php
+++ php/debug/executionTimeOfFunction.php
@@ -5,5 +5,5 @@ function executionTimeOfFunction (): string
     $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
     $time = $time / 60;
 
-    return "O script executou em $time minutos.";
+    return "The script executed in $time minutes.";
 }
